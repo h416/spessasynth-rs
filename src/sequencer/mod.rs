c968c819723e78d2e922bb9bf5328939c69e85cb
h416 <4962743+h416@users.noreply.github.com>
2026-07-04 06:06:05 +0900

    refactor: restructure sequencer to match 4.3.0 layout

--- src/sequencer/mod.rs
+++ src/sequencer/mod.rs
@@ -1,7 +1,7 @@
 pub mod types;
 #[allow(clippy::module_inception)]
 pub mod sequencer;
-pub mod song_control;
+pub mod load_new_sequence;
 pub mod process_event;
 pub mod process_tick;
-pub mod play;
+pub mod set_time_to;
